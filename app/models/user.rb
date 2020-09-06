f1294ba93ade36c19cc8bcbc600798c4d4710b73
minato0207 <kota0207xx@icloud.com>
2020-09-06 16:46:48 +0900

    トップページ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,23 +6,39 @@ class User < ApplicationRecord
 
    
 
-  with_options presence: {message "が空です！"} do
+  # with_options presence: {message "が空です！"} do
    
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-      validates :first_mane
-      validates :last_name
-      validates :first_mane_kana
-      validates :last_name_kana
-     end
+  #   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  #     validates :first_mane
+  #     validates :last_name
+  #     validates :first_mane_kana
+  #     validates :last_name_kana
+  #   end
 
-   validates :nickname, presence: {message "が空です！"}
-   validates :email, presence: {message "が空です！"}
-   validates :password, presence: {message "が空です！"}
-   validates :birthday, presence: {message "が空です！"}
-  end
-end
+  #   validates :nickname
+  #   validates :email
+  #   validates :password, presence: {message "が空です！"},length: { minimum: 6}
+  #   validates :birthday
+  # end
 
-   
+
+  validates :nickname, :first_name, :last_name, :birthday, presence: true
+  validates :password, presence: true, length: { minimum: 7 }, 
+            # 英数字のみ可
+            format: { with: /\A[a-z0-9]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }
+  validates :email, presence: true, 
+            # 重複不可
+            uniqueness: { case_sensitive: false }, 
+            # 英数字のみ可,@を挟んだemailの形になっているか
+            # /^\S+@\S+\.\S+$/   /\A[a-z0-9]+\z/i  /^[a-zA-Z0-9]+$/
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }
+  validates :first_mane_kana, :last_name_kana, presence: true, 
+            # カナのみ可
+            format: { with: /\A([ァ-ン]|ー)+\z/, message: "is must NOT contain any other characters than alphanumerics." }
+            has_many :sns_credentials
+
+
+
 
 
 
