0543de08e8fe0d06882954f70bc1698a1e4be276
minato0207 <kota0207xx@icloud.com>
2020-09-06 15:46:50 +0900

    validatesの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,39 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   
+
+  with_options presence: {message "が空です！"} do
+   
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+      validates :first_mane
+      validates :last_name
+      validates :first_mane_kana
+      validates :last_name_kana
+     end
+
+   validates :nickname, presence: {message "が空です！"}
+   validates :email, presence: {message "が空です！"}
+   validates :password, presence: {message "が空です！"}
+   validates :birthday, presence: {message "が空です！"}
+  end
+end
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
