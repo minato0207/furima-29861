335b5c2d5baeb6532bbc995291d7482f011b105b
minato0207 <kota0207xx@icloud.com>
2020-09-08 18:19:40 +0900

    ユーザー管理機能編集修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configre_permitted_parameters, if: :devise_controller?
 
+
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :last_name, :first_name, :last_name_kana, :first_name_kana])
   end
-
-
-
-  
-  
 
   private
 
@@ -20,6 +16,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  
   
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,3 @@
 class ItemsController < ApplicationController
-  def index
   
-  end
-
-  def new
-  end
-
-
-
 end
