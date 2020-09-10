983c53cbed5109330f2ebee9b0896ab659f282f4
minato0207 <kota0207xx@icloud.com>
2020-09-10 11:29:40 +0900

    非同期通信実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
+    name                      {Faker::Name.initials}
+    category_id               {rand(1..10)}
+    price                     {rand(300..9999999)}
+    text                      {"aaaaaaaaaaaaaaaaa"}
+    status_id                 {rand(1..6)}
+    area_id                   {rand(1..47)}
+    day_standard_id           {rand(1..3)}
+    delivery_fee_id           {rand(1..2)}
     
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品がうまくいくとき' do
+    context '商品が出品できるとき' do
+      it "nameとprice、textとarea、day_standardとcategory、delivery_feeとstatus、imageが存在すれば登録できる" do
+        expect(@item).to be_valid
+      end
+      
+
+      context '新規登録がうまくいかないとき' do
+        it "nicknameが空だと登録できない" do
+        end
+        it "nicknameが7文字以上であれば登録できない" do
+        end
+        it "emailが空では登録できない" do
+        end
+        it "重複したemailが存在する場合登録できない" do
+        end
+        it "passwordが空では登録できない" do
+        end
+        it "passwordが5文字以下であれば登録できない" do
+        end
+        it "passwordが存在してもpassword_confirmationが空では登録できない" do
+       end
+      end
+    end
+  end
 end
+
