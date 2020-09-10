e8384e318f368b1952a81df8fc3dfea471a83501
minato0207 <kota0207xx@icloud.com>
2020-09-10 13:55:24 +0900

    商品出品機能実装修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,39 +28,69 @@ describe Item do
           @item.valid?
           expect(@item.errors.full_messages).to include("Area can't be blank", "Area is not a number")
         end
-        it "day_standard_idとcategory_idが空だと登録できない" do
+        it "day_standard_idが空だと登録できない" do
           @item.day_standard_id = ""
           @item.category_id = ""
           @item.valid?
-          expect(@item.errors.full_messages).to include("Day standard can't be blank", "Category can't be blank", "Day standard is not a number", "Category is not a number")
+          expect(@item.errors.full_messages).to include("Day standard can't be blank", "Day standard is not a number", )       
         end
-        it "delivery_fee_idとstatus_idが空だと登録できない" do
+        it "category_idが空だと登録できない"do
+        @item.category_id = ""
+        @item.valid?
+          expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+        end
+        it "delivery_fee_idが空だと登録できない" do
           @item.delivery_fee_id = ""
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Delivery fee can't be blank","Delivery fee is not a number")
+        end
+        it "status_idが空だと登録できない"do
           @item.status_id = ""
           @item.valid?
-          expect(@item.errors.full_messages).to include("Delivery fee can't be blank", "Status can't be blank", "Delivery fee is not a number", "Status is not a number")
+          expect(@item.errors.full_messages).to include( "Status can't be blank", "Status is not a number")
         end
-        it "area_idとday_standard_idとcategory_id, delivery_fee_idとstatus_idのidが0だと登録できない" do
-          @item.area_id = "0"
-          @item.day_standard_id = "0"
-          @item.category_id = "0"
-          @item.delivery_fee_id = "0"
-          @item.status_id = "0"
+        it "area_idとstatus_idのidが0だと登録できない" do
+          @item.area_id = "0"          
           @item.valid?
-          expect(@item.errors.full_messages).to include("Area must be other than 0",
-            "Day standard must be other than 0",
-            "Category must be other than 0",
-            "Delivery fee must be other than 0",
-            "Status must be other than 0")
+          expect(@item.errors.full_messages).to include("Area must be other than 0")
+        end
+        it "day_standard_idのidが0だと登録できない"do
+          @item.day_standard_id = "0"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Day standard must be other than 0")
+        end
+        it "category_idのidが0だと登録できない"do
+          @item.category_id = "0"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Category must be other than 0")
+        end
+          it "delivery_fee_idのidが0だと登録できない"do
+          @item.delivery_fee_id = "0"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Delivery fee must be other than 0")
+        end
+        it "status_idのidが0だと登録できない"do
+        @item.status_id = "0"
+        @item.valid?
+          expect(@item.errors.full_messages).to include("Status must be other than 0")
         end
         it "priceが300から10000000の間でなくては保存できない" do
           @item.price = "200"
           @item.valid?
           expect(@item.errors.full_messages).to include("Price must be greater than 300")
         end 
+        it "priceが300から10000000の間でなくては保存できない" do
+          @item.price = "200"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price must be greater than 300")
+        end
+        it "priceが300から10000000の間でなくては保存できない" do
+          @item.price = "10000000"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+        end
       end
     end
   end
-
 end
 
