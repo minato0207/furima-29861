b2d473f296c3f0a33b8a80d36c6a5f69844ba9b0
minato0207 <kota0207xx@icloud.com>
2020-09-16 18:04:24 +0900

    Update 20200908102212_create_items.rb

--- db/migrate/20200908102212_create_items.rb
+++ db/migrate/20200908102212_create_items.rb
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :area_id, null: false
       t.integer :day_standard_id, null: false
       t.integer :delivery_fee_id, null: false
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
