10a53670726cbfff0826b80985e1615e3d1bba65
minato0207 <kota0207xx@icloud.com>
2020-09-16 14:48:59 +0900

    商品購入機能実装修正

--- app/forms/adrress_order.rb
+++ app/forms/adrress_order.rb
@@ -1,5 +1,4 @@
 class AdrressOrder
-  attr_accessor :token
   include ActiveModel::Model
   attr_accessor :zip, :area_id,:city,:address,:build_name,:phone_call,:token,:user_id, :item_id
   with_options presence: true do
@@ -7,7 +6,6 @@ class AdrressOrder
     validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city, :address
-
   end
 
 
